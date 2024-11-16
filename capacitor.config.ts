677f7321919112de53d1a7a363055d4940b4a745
edison <eddytiger7@gmail.com>
2024-11-16 20:04:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic_Expense_Tracker',
+  webDir: 'www'
+};
+
+export default config;
